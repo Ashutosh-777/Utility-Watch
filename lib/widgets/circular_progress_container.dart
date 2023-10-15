4785689afee5ff192f2f7c6e2a9d5f77cad4dc6e
Vijay Mehrotra <vijaymehrotra99@gmail.com>
2023-10-15 19:34:35 +0530

    Made the objective screen scrollable for small..

--- lib/widgets/circular_progress_container.dart
+++ lib/widgets/circular_progress_container.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
-
 import 'package:utility_watch/globals/colors.dart';
 
 class MyPercentageContainer extends StatelessWidget {
-  final String heading;
-  final String percentage;
+  String heading;
+  String percentage;
 
-  const MyPercentageContainer({
+  MyPercentageContainer({
     Key? key,
     required this.heading,
     required this.percentage,
@@ -16,47 +15,52 @@ class MyPercentageContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Expanded(
-        child: Container(
-          width: (MediaQuery.of(context).size.width - 60) / 2,
-          padding: EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(20),
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      width: (MediaQuery.of(context).size.width - 80) / 2,
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 3),
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: Text(
-                  heading,
-                  style: GoogleFonts.nunitoSans(
-                    fontSize: 21,
-                    fontWeight: FontWeight.bold,
-                    color: MyColors.greyColor,
-                  ),
-                ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: Text(
+              heading,
+              style: GoogleFonts.nunitoSans(
+                fontSize: 21,
+                fontWeight: FontWeight.bold,
+                color: MyColors.greyColor,
               ),
-              SizedBox(height: 10),
-              Center(
-                child: CircularPercentIndicator(
-                  radius: 50.0,
-                  lineWidth: 13.0,
-                  animation: true,
-                  percent: 0.7,
-                  center: new Text(
-                    percentage,
-                    style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-                  ),
-                  circularStrokeCap: CircularStrokeCap.round,
-                  progressColor: MyColors.blueColor,
-                ),
-              ),
-              SizedBox(height: 10),
-            ],
+            ),
           ),
-        ),
+          SizedBox(height: 10),
+          Center(
+            child: CircularPercentIndicator(
+              radius: 50.0,
+              lineWidth: 13.0,
+              animation: true,
+              percent: 0.7,
+              center: Text(
+                percentage,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+              ),
+              circularStrokeCap: CircularStrokeCap.round,
+              progressColor: MyColors.blueColor,
+            ),
+          ),
+          SizedBox(height: 10),
+        ],
       ),
     );
   }
